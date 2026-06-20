b06b6dbab3dee0f02299727515a9008134b9bdf2
MorsH14 <midemorsh@gmail.com>
2026-06-20 06:37:24 +0100

    Make home page public and show all listings for all users

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-import { useLocalStorage } from "@/utils/hooks/useLocalStorage";
-import { initialUserData } from "@/db";
 import HomeClient from "./HomeClient";
-import HomeWorker from "./HomeWorker";
 
 export default function Homepage() {
-  const [user] = useLocalStorage("rana-user-profile", initialUserData);
-  return user.accountType === "client" ? <HomeClient /> : <HomeWorker />;
+  return <HomeClient />;
 }
